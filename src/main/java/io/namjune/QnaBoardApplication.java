ed257460dd3f9e229f74ca49ebd06fb83fee5960
Namjun Kim <namjunemy@gmail.com>
2018-06-07 02:18:31 +0900

    feat: Project setting, connect sourcetree

---
+++ src/main/java/io/namjune/QnaBoardApplication.java
@@ -0,0 +1,12 @@
+package io.namjune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaBoardApplication.class, args);
+	}
+}
---
+++ src/test/java/io/namjune/QnaBoardApplicationTests.java
@@ -0,0 +1,16 @@
+package io.namjune;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QnaBoardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
