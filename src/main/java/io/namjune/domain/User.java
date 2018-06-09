00e94678031676fa183b90d613cda729cf1bf60c
Namjun Kim <namjunemy@gmail.com>
2018-06-10 02:35:01 +0900

    feat: JPA를 이용하여 자바 객체와 테이블 매핑, Collections => JPA 데이터 처리 방식 변경

--- src/main/java/io/namjune/domain/User.java
+++ src/main/java/io/namjune/domain/User.java
@@ -1,7 +1,19 @@
-package io.namjune.web;
+package io.namjune.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+  @Id
+  @GeneratedValue
+  private Long id;
+  
+  @Column(nullable=false, length=20)
   private String userId;
+  
   private String password;
   private String name;
   private String email;
--- src/main/java/io/namjune/web/UserController.java
+++ src/main/java/io/namjune/web/UserController.java
@@ -1,26 +1,28 @@
 package io.namjune.web;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import io.namjune.domain.User;
+import io.namjune.domain.UserRepository;
 
 @Controller
 public class UserController {
-  private List<User> users = new ArrayList<User>();
-  
+  @Autowired
+  private UserRepository userRepository;
+
   @PostMapping("/create")
   public String create(User user) {
-    users.add(user);
+    userRepository.save(user);
     System.out.println(user);
     return "redirect:/list";
   }
-  
+
   @GetMapping("/list")
   public String list(Model model) {
-    model.addAttribute("users", users);
+    model.addAttribute("users", userRepository.findAll());
     return "list";
   }
 }
