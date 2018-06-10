dbade389d210696beef3eacc9ac13296ebb3a6c8
Namjun Kim <namjunemy@gmail.com>
2018-06-11 01:45:28 +0900

    feat: 사용자 정보 수정 form 페이지 추가, 컨트롤러 매핑 추가 및 연동

--- src/main/java/io/namjune/web/UserController.java
+++ src/main/java/io/namjune/web/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import io.namjune.domain.User;
@@ -32,5 +33,11 @@ public class UserController {
   public String list(Model model) {
     model.addAttribute("users", userRepository.findAll());
     return "/user/list";
+  }
+
+  @GetMapping("{id}/form")
+  public String updateForm(@PathVariable Long id, Model model) {
+    model.addAttribute("user", userRepository.findOne(id));
+    return "/user/updateForm";
   }
 }
