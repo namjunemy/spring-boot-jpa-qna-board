dab7244d6806c0c89548fe773edf645c6879b76c
Namjun Kim <namjunemy@gmail.com>
2018-06-11 01:16:57 +0900

    feat: HTML, URL 정리, 컨트롤러 추가 및 연동

--- src/main/java/io/namjune/web/UserController.java
+++ src/main/java/io/namjune/web/UserController.java
@@ -5,24 +5,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import io.namjune.domain.User;
 import io.namjune.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+
   @Autowired
   private UserRepository userRepository;
 
-  @PostMapping("/create")
+  @GetMapping("/form")
+  public String form() {
+    return "/user/form";
+  }
+
+  @PostMapping("")
   public String create(User user) {
     userRepository.save(user);
     System.out.println(user);
-    return "redirect:/list";
+    return "redirect:/users";
   }
 
-  @GetMapping("/list")
+  @GetMapping("")
   public String list(Model model) {
     model.addAttribute("users", userRepository.findAll());
-    return "list";
+    return "/user/list";
   }
 }
