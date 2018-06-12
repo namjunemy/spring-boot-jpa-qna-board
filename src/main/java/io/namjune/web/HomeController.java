844fca97942e0ead1a3befa2797d653f0a1139f8
Namjun Kim <namjunemy@gmail.com>
2018-06-13 03:06:28 +0900

    feat: 4-5 질문목록 기능 구현

--- src/main/java/io/namjune/web/HomeController.java
+++ src/main/java/io/namjune/web/HomeController.java
@@ -1,14 +1,22 @@
 package io.namjune.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import io.namjune.domain.QuestionRepository;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+  @Autowired
+  private QuestionRepository questionRepository;
+
   @GetMapping("")
-  public String home() {
+  public String home(Model model) {
+    model.addAttribute("questions", questionRepository.findAll());
     return "index";
   }
 }
