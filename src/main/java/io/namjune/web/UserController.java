62a094cbe54018d83b3f493c8b15766394899824
Namjun Kim <namjunemy@gmail.com>
2018-06-13 21:44:37 +0900

    fix: 개인정보 수정시 세션에 등록된 유저 체크하는 기능 오류 수정

--- src/main/java/io/namjune/web/UserController.java
+++ src/main/java/io/namjune/web/UserController.java
@@ -70,7 +70,7 @@ public class UserController {
 
   @GetMapping("{id}/form")
   public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
-    if (HttpSessionUtils.isLoginUser(session)) {
+    if (!HttpSessionUtils.isLoginUser(session)) {
       return "redirect:/users/loginForm";
     }
 
@@ -86,7 +86,7 @@ public class UserController {
 
   @PutMapping("/{id}")
   public String update(@PathVariable Long id, User updateUser, HttpSession session) {
-    if (HttpSessionUtils.isLoginUser(session)) {
+    if (!HttpSessionUtils.isLoginUser(session)) {
       return "redirect:/users/loginForm";
     }
 
