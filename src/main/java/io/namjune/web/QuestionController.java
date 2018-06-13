9a80037be50eae54afef7e394eb101c7f26ace8f
Namjun Kim <namjunemy@gmail.com>
2018-06-13 23:08:52 +0900

    feat: 5-2 질문 상세보기 기능 구현

--- src/main/java/io/namjune/web/QuestionController.java
+++ src/main/java/io/namjune/web/QuestionController.java
@@ -3,7 +3,9 @@ package io.namjune.web;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import io.namjune.domain.Question;
@@ -37,5 +39,12 @@ public class QuestionController {
     questionRepository.save(newQuestion);
 
     return "redirect:/";
+  }
+
+  @GetMapping("/{id}")
+  public String detail(@PathVariable Long id, Model model) {
+    model.addAttribute("question", questionRepository.findOne(id));
+    
+    return "/qna/show";
   }
 }
