216b1c1b659f3ff5d4ec125d225fcda1a4121184
Namjun Kim <namjunemy@gmail.com>
2018-06-13 23:50:18 +0900

    feat: 5-3 질문 수정, 삭제 기능 구현

--- src/main/java/io/namjune/domain/Question.java
+++ src/main/java/io/namjune/domain/Question.java
@@ -42,4 +42,9 @@ public class Question {
 
     return createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
   }
+
+  public void update(String title, String contents) {
+    this.title = title;
+    this.contents = contents;
+  }
 }
--- src/main/java/io/namjune/web/QuestionController.java
+++ src/main/java/io/namjune/web/QuestionController.java
@@ -4,9 +4,11 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import io.namjune.domain.Question;
 import io.namjune.domain.QuestionRepository;
@@ -42,9 +44,29 @@ public class QuestionController {
   }
 
   @GetMapping("/{id}")
-  public String detail(@PathVariable Long id, Model model) {
+  public String show(@PathVariable Long id, Model model) {
     model.addAttribute("question", questionRepository.findOne(id));
-    
+
     return "/qna/show";
+  }
+
+  @GetMapping("/{id}/form")
+  public String updateForm(@PathVariable Long id, Model model) {
+    model.addAttribute("question", questionRepository.findOne(id));
+    return "/qna/updateForm";
+  }
+
+  @PutMapping("/{id}")
+  public String update(@PathVariable Long id, String title, String contents) {
+    Question question = questionRepository.findOne(id);
+    question.update(title, contents);
+    questionRepository.save(question);
+    return String.format("redirect:/questions/%d", id);
+  }
+
+  @DeleteMapping("/{id}")
+  public String delete(@PathVariable Long id) {
+    questionRepository.delete(id);
+    return "redirect:/";
   }
 }
