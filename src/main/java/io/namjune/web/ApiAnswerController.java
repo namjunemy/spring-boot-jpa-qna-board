b9eb1ef6aed6b6e9b6cdf061870861689447f5c0
Namjun Kim <namjunemy@gmail.com>
2018-06-15 00:21:23 +0900

    feat: 6-1 jquery, ajax, json api를 이용하여 답변 기능 구현

--- src/main/java/io/namjune/web/ApiAnswerController.java
+++ src/main/java/io/namjune/web/ApiAnswerController.java
@@ -2,19 +2,19 @@ package io.namjune.web;
 
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import io.namjune.domain.Answer;
 import io.namjune.domain.AnswerRepository;
 import io.namjune.domain.Question;
 import io.namjune.domain.QuestionRepository;
 import io.namjune.domain.User;
 
-@Controller
-@RequestMapping("/questions/{questionId}/answers")
-public class AnswerController {
+@RestController
+@RequestMapping("/api/questions/{questionId}/answers")
+public class ApiAnswerController {
 
   @Autowired
   private AnswerRepository answerRepository;
@@ -23,16 +23,14 @@ public class AnswerController {
   private QuestionRepository questionRepository;
 
   @PostMapping("")
-  public String create(@PathVariable Long questionId, String contents, HttpSession session) {
+  public Answer create(@PathVariable Long questionId, String contents, HttpSession session) {
     if (!HttpSessionUtils.isLoginUser(session)) {
-      return "/users/loginForm";
+      return null;
     }
 
     User loginUser = HttpSessionUtils.getUserFormSession(session);
     Question question = questionRepository.findOne(questionId);
     Answer answer = new Answer(loginUser, question, contents);
-    answerRepository.save(answer);
-
-    return String.format("redirect:/questions/%d", questionId);
+    return answerRepository.save(answer);
   }
 }
